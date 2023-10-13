842bea903e55678ea3f1fd96f9b311f5a7534358
Your Nameeli <elighidiu@yahoo.com>
2023-10-13 12:24:17 +0200

    prev & next review

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,29 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 const App = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
-  console.log(name);
+
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  };
+
+  const nextPerson = () => {
+    setIndex((curIndex) => {
+      const newIndex = curIndex + 1;
+      return checkNumber(newIndex);
+    });
+  };
+  const prevPerson = () => {
+    setIndex((curIndex) => {
+      const newIndex = curIndex - 1;
+      return checkNumber(newIndex);
+    });
+  };
 
   return (
     <main>
@@ -19,6 +41,16 @@ const App = () => {
         <h4 className="author">{name}</h4>
         <p className="job">{job}</p>
         <p className="info">{text}</p>
+        <div>
+          <div className="btn-container">
+            <button type="button" className="prev-btn" onClick={prevPerson}>
+              <FaChevronLeft />
+            </button>
+            <button type="button" className="next-btn" onClick={nextPerson}>
+              <FaChevronRight />
+            </button>
+          </div>
+        </div>
       </article>
     </main>
   );
