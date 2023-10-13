9a84d7a6d968e0d791e81a243b18e442e7de1e95
Your Nameeli <elighidiu@yahoo.com>
2023-10-13 12:31:47 +0200

    random review

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,14 @@ const App = () => {
     });
   };
 
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    setIndex(checkNumber(randomNumber));
+  };
+
   return (
     <main>
       <article className="review">
@@ -51,6 +59,9 @@ const App = () => {
             </button>
           </div>
         </div>
+        <button className="btn btn-hipster" onClick={randomPerson}>
+          Random review
+        </button>
       </article>
     </main>
   );
